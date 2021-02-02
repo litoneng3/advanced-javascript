229b9d27f22035f6c6c987f71deaf2beca605df3
litoneng3 <47786100+litoneng3@users.noreply.github.com>
2021-02-02 13:49:01 +0600

    I am advanced now.

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//Falsy:
+//false
+//0
+//'' empty string
+//undefined
+//null
+
+//Truthy:
+//'0'
+//' ', [], {}
+
+
+const name = { };
+if(name){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
